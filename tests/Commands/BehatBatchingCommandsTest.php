778b71bd65c1b3563b12e3573a9563aea57c98a9
Evgenii Nikitin <yenyasinn@gmail.com>
2021-09-21 17:24:01 +0300

    EWPP-1475: Separate test cases executions.

--- tests/Commands/BehatBatchingCommandsTest.php
+++ tests/Commands/BehatBatchingCommandsTest.php
@@ -11,30 +11,34 @@ use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\BufferedOutput;
 
 /**
- * Test project symlink command.
+ * Test "Ensure Behat batching" command.
+ *
+ * Task runner is run in different tests to avoid wrong execution.
  */
 class BehatBatchingCommandsTest extends TestCase
 {
     /**
-     * Test "behat:ensure-batching" command.
+     * Test "behat:ensure-batching" command with correct features.
      */
-    public function testBehatBatchingCommands()
+    public function testCorrectPath()
     {
-        $output = new BufferedOutput();
-
         // Run command on the path with correct Behat features.
-        $this->runCommand(__DIR__ . "/../fixtures/correct", $output);
-        $text = $output->fetch();
-        $this->assertStringContainsString('[OK] All scenarios have been assigned to a test batch.', $text);
+        $result = $this->runBehatEnsureBatchingCommand(__DIR__ . "/../fixtures/correct");
+        $this->assertStringContainsString('[OK] All scenarios have been assigned to a test batch.', $result);
+    }
 
+    /**
+     * Test "behat:ensure-batching" command with wrong features.
+     */
+    public function testWrongPath()
+    {
         // Run command on the path with wrong Behat features.
-        $this->runCommand(__DIR__ . "/../fixtures/wrong", $output);
-        $text = $output->fetch();
-        $this->assertStringContainsString('[ERROR] The following scenarios have not been assigned to a test batch:', $text);
-        $this->assertStringContainsString('Tag is absent', $text);
-        $this->assertStringContainsString('Incomplete tag', $text);
-        $this->assertStringContainsString('Typo in the tag', $text);
-        $this->assertStringContainsString('Special characters aren\'t allowed in the tag', $text);
+        $result = $this->runBehatEnsureBatchingCommand(__DIR__ . "/../fixtures/wrong");
+        $this->assertStringContainsString('[ERROR] The following scenarios have not been assigned to a test batch:', $result);
+        $this->assertStringContainsString('Tag is absent', $result);
+        $this->assertStringContainsString('Incomplete tag', $result);
+        $this->assertStringContainsString('Typo in the tag', $result);
+        $this->assertStringContainsString('Special characters aren\'t allowed in the tag', $result);
     }
 
     /**
@@ -42,14 +46,18 @@ class BehatBatchingCommandsTest extends TestCase
      *
      * @param string $path
      *   Path to test.
-     * @param \Symfony\Component\Console\Output\BufferedOutput $output
+     *
+     * @return string
      *   Console output.
      */
-    protected function runCommand(string $path, BufferedOutput $output): void
+    protected function runBehatEnsureBatchingCommand(string $path): string
     {
+        $output = new BufferedOutput();
         $input = new StringInput("behat:ensure-batching --path={$path}");
         $runner = new TaskRunner($input, $output, $this->getClassLoader());
         $runner->run();
+
+        return $output->fetch();
     }
 
     /**
